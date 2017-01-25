a65a0db203c0bebd0b46907e4556bd439c79b47e
Bill Mayer <globvious@gmail.com>
2017-01-24 19:58:50 -0500

    add test and reference files

---
+++ automated_tests/rowsdowr/tests/functional/rowsdowr.js
@@ -0,0 +1,30 @@
+define(function(require) {
+
+    var registerSuite = require('intern!object'),
+        assert = require('intern/chai!assert'),
+        fs = require('intern/dojo/node!fs'),
+
+        commonObj = require('../../../page_objects/commonObj'),
+        rowsdowrObj = require('../../../page_objects/rowsdowrObj');
+
+    registerSuite({
+        name: 'Rowsdowr',
+
+        setup: function() {
+            contents = fs.readFileSync('tests/resources/info.json');
+            info = JSON.parse(contents);
+
+            commonObj = new commonObj(this.remote);
+            rowsdowrObj = new rowsdowrObj(this.remote);
+        },
+
+        // Load web page
+        loadWebpage: function() {
+            return commonObj.loadPage(info.loadUrl1)
+            .then(function(url) {
+                assert.include(url, info.assertUrl1);
+            });
+        },
+
+    });
+});
